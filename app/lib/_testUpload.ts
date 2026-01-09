ab37380e73c677ec3ebc9835a9dedf253337d152
lcmaurya <lal7590@gmail.com>
2026-01-09 11:16:25 +0530

    remove test upload debug file (_testUpload.ts)

--- app/lib/_testUpload.ts
+++
@@ -1,7 +0,0 @@
-import { uploadSkillFile } from "./uploadSkillMedia";
-
-(async () => {
-  const file = new File(["HELLO"], "test.txt", { type: "text/plain" });
-  const url = await uploadSkillFile("debug-user", "debug-skill", file, "images");
-  console.log("UPLOAD OK:", url);
-})();
